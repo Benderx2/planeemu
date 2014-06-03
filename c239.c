adeaedd5c065997c7178b37539942436b7f522d4
Benderx2 <excectra@gmail.com>
2014-06-03 18:51:06 +0800

    Update c239.c

--- c239.c
+++ c239.c
@@ -1,9 +1,9 @@
-/* 747 Flight Simulator */
+/* C239 Flight Simulator */
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
-#include <747.h>
+#include <C239.h>
 #include <stdbool.h>
 int main(int argc, const char* argv[])
 {
